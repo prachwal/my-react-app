b0da6c99b7145637629e38d4b2b6e0c7f4fa7c79
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2025-03-02 20:36:24 +0100

    add helmet

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,31 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { HelmetProvider, Helmet } from "react-helmet-async"; // Importujemy HelmetProvider i Helmet
 import "./utils/i18nNew"; // Importujemy konfigurację i18n
+import { useTranslation } from "react-i18next"; // Importujemy hook useTranslation
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+
+const RootComponent = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <HelmetProvider>
+          <Helmet>
+            <title>{t("Title")}</title>
+            <meta name="description" content={t("Description") ?? ""} />
+            <html lang={i18n.language} />
+          </Helmet>
+          <App />
+        </HelmetProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+root.render(<RootComponent />);
